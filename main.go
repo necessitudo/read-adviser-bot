d8455729f2b9c0ddfdd96e68ade60f29ea144f7d
necessitudo <interrogatus@gmail.com>
2024-01-06 17:54:17 +0300

    get token

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+
+	token = mustToken()
+
+	//tgClient = telegram.New(token)
+
+	//fetcher = fetcher.New(tgClient)
+
+	//processor = processor.New(tgClient)
+
+	//consumer.Start(fetcher, processor)
+
+}
+
+func mustToken() string {
+
+	token := flag.String(
+		"token-bot-token",
+		"",
+		"token for access to telegram bot")
+
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+
+	}
+	return *token
 
 }
